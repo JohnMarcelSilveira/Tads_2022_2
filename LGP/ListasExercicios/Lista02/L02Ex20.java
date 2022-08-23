a2da4cc7c550a501ceec0bab22c77a16b3be1388
John Marcel Silveira <marcel-eu@hotmail.com>
2022-08-23 16:47:24 -0300

    Master

---
+++ LGP/ListasExercicios/Lista02/L02Ex20.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class L02Ex20 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        final float VALOR_GASOLINA = 4.599f;
+        float odometroInicial, odometroFinal, litrosGastos, valorTotalRecebido;
+    }
+}
