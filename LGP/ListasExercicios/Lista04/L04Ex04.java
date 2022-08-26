126329ef02985f6474cd428f8d2e9a293c04281f
Marcel Silveira <marcel-eu@hotmail.com>
2022-08-25 21:36:58 -0300

    master

---
+++ LGP/ListasExercicios/Lista04/L04Ex04.java
@@ -0,0 +1,45 @@
+package LGP.ListasExercicios.Lista04;
+
+import java.util.Scanner;
+
+public class L04Ex04 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        float saldo,saque,deposito;
+        char opcao;
+
+        final char CONSULTA_SALDO='a';
+        final char SAQUE='b';
+        final char DEPOSITO='c';
+        final char SAIR='d';
+
+        saldo=0f;
+
+        do{
+            System.out.println("\nDigite um opção\n(a) consulta saldo\n(b) saque\n(c) depósito\n(d) sair");
+            opcao = in.next().charAt(0);
+            switch(opcao){
+                case 'a': {
+                    System.out.printf("Seu saldo é R$%.2f\n",saldo);
+                    break;
+                }
+                case 'b': {
+                    System.out.println("Quanto você quer sacar?");
+                    saque = in.nextFloat();
+                    if(saldo-saque<0){
+                        System.out.println("Você não tem saldo para fazer esse saque");
+                    }else{
+                        saldo -= saque;
+                    }
+                    break;
+                }
+                case 'c':{
+                    System.out.println("Quanto você quer depositar?");
+                    saldo += in.nextFloat();
+                    break;
+                }
+            }
+        }while(opcao!='d');
+
+    }
+}
