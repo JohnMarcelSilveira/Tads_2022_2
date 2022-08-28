da5b0f8a99dff3fed2eb5a7bef605d3b78e7111b
John Marcel Silveira <marcel-eu@hotmail.com>
2022-08-28 17:49:35 -0300

    master

--- LGP/ListasExercicios/Lista04/L04Ex13.java
+++ LGP/ListasExercicios/Lista04/L04Ex13.java
@@ -3,31 +3,25 @@ import java.util.Scanner;
 public class L04Ex13 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        float nota1,nota2,nota3,maior1,maior2;
-        int cont = 0;
-        nota1=nota2=nota3=maior1=maior2=0f;
-        while(cont<3){
-            cont++;
-            System.out.print("Digite a "+cont+"ª nota:");
-            if(cont==1){
-                nota1 = in.nextFloat();
-                maior1 = nota1;
-            }else if(cont==2){
-                nota2 = in.nextFloat();
-                if(nota2>maior1 || maior1 == nota2){
-                    maior2 = nota2;
-                }
-            }else{
-                nota3 = in.nextFloat();
-                if(nota3 > maior2){
-                    maior2 = nota3;
-                }else if(nota3 > maior1){
-                    maior1 = nota3;
-                }
-            }
+        float nota1, nota2, nota3, media, menor;
+        System.out.print("Digite a 1ª nota:");
+        nota1 = in.nextFloat();
+        System.out.print("Digite a 2ª nota:");
+        nota2 = in.nextFloat();
+        System.out.print("Digite a 3ª nota:");
+        nota3 = in.nextFloat();
+
+        if (nota1 < nota2) {
+            menor = nota1;
+        } else {
+            menor = nota2;
         }
-        
-        System.out.println(maior1 +"\n"+ maior2);
+        if (nota3 < menor) {
+            menor = nota3;
+        }
+
+        media = ((nota1 + nota2 + nota3) - menor) / 2f;
+        System.out.println(media);
 
     }
 }
