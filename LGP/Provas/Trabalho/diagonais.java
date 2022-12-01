25a13c0f31685d57bd4bfef2261fcc4a4a0b3226
John Marcel Silveira <marcel-eu@hotmail.com>
2022-12-01 09:27:42 -0300

    Master trabalho final ajustes de layout

--- LGP/Provas/Trabalho/diagonais.java
+++ LGP/Provas/Trabalho/diagonais.java
@@ -1,5 +1,19 @@
-import javax.swing.JPanel;
+/*
+ * Faça um programa com uma matriz [8][8] que preencha cada uma das diagonais dela de forma
+consecutiva e automatizada, utilizando um valor único para cada diagonal, e que faça com que após
+o preenchimento, os valores sejam atualizados para o valor da próxima diagonal. Escolha e informe
+ao usuário uma tecla que será utilizada para a finalização (enquanto o usuário não finalizar, continua
+andando com as diagonais). Por exemplo, em uma matriz 4x4:
+     1  2  3  4     2  3  4  1
+     2  3  4  1  →  3  4  1  2
+     3  4  1  2     4  1  2  3
+     4  1  2  3     1  2  3  4
 
+     https://www.youtube.com/watch?v=AL4nseu7sRE
+ */
+
+
+import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.*;
--- LGP/Provas/Trabalho/trabalhoFinal.java
+++ LGP/Provas/Trabalho/trabalhoFinal.java
@@ -1,22 +1,9 @@
-/*
- * Faça um programa com uma matriz [8][8] que preencha cada uma das diagonais dela de forma
-consecutiva e automatizada, utilizando um valor único para cada diagonal, e que faça com que após
-o preenchimento, os valores sejam atualizados para o valor da próxima diagonal. Escolha e informe
-ao usuário uma tecla que será utilizada para a finalização (enquanto o usuário não finalizar, continua
-andando com as diagonais). Por exemplo, em uma matriz 4x4:
-     1  2  3  4     2  3  4  1
-     2  3  4  1  →  3  4  1  2
-     3  4  1  2     4  1  2  3
-     4  1  2  3     1  2  3  4
 
-     https://www.youtube.com/watch?v=AL4nseu7sRE
- */
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.FlowLayout;
 
 public class trabalhoFinal extends JFrame implements ActionListener {
 
@@ -28,14 +15,14 @@ public class trabalhoFinal extends JFrame implements ActionListener {
         btnExercicio2 = new JButton("Exercicio 2");
         btnExercicio3 = new JButton("Exercicio 3");
 
-        JFrame frame = new JFrame("Exercicio 3 Logica");
-        frame.setSize(600, 600);
+        JFrame frame = new JFrame("Trabalho Final Logica");
+        frame.setSize(500, 400);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
 
-        btnExercicio2.setBounds(20, 20, 100, 20);
-        btnExercicio3.setBounds(20, 100, 100, 20);
+        btnExercicio2.setBounds(20, 70, 100, 20);
+        btnExercicio3.setBounds(20, 150, 100, 20);
 
         frame.add(btnExercicio2);
         frame.add(btnExercicio3);
@@ -47,13 +34,11 @@ public class trabalhoFinal extends JFrame implements ActionListener {
         ActionListener al = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (btnExercicio3.getModel().isArmed()) {
-                    System.out.println("entrei aqui para iniciar o diagonal.");
                     diagonais diagonal = new diagonais();
                     diagonal.setBounds(0, 0, 600, 600);
                     frame.add(diagonal);
                 }
                 if (btnExercicio2.getModel().isArmed()) {
-                    System.out.println("entrei aqui botao exercicio 2.");
                     quadrado quadrado = new quadrado();
                     quadrado.setBounds(0, 0, 600, 600);
                     frame.add(quadrado);
